86ee108b152348392405cc9ee449f5e4bfde9bfc
FarzamAlam <farzamcse@gmail.com>
2020-01-26 13:27:27 +0530

    Created a basic REST with Get and Post methods

--- src/main.go
+++ src/main.go
@@ -3,13 +3,44 @@ package main
 import (
 	"log"
 	"net/http"
-	"shorturl"
+
+	"github.com/gorilla/mux"
 )
 
+func home(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "application/json")
+	switch r.Method {
+	case "GET":
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"message":"GET Called!"}`))
+
+	case "POST":
+		w.WriteHeader(http.StatusCreated)
+		w.Write([]byte(`{"message":"POST Called!"}`))
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte(`{"message":"Not Found!"}`))
+	}
+}
 func main() {
 
-	handler := shorturl.RedirectHandler()
+	// handler := shorturl.RedirectHandler()
 
-	log.Fatal(http.ListenAndServe(":8080", handler))
-	//_ = shorturl.CreateURL("2a419415", "https://www.flipkart.com/mens-clothing/sports-wear/caps/pr?sid=2oq%2Cs9b%2C6gr%2Ch2i&p%5B%5D=facets.fulfilled_by%255B%255D%3DFlipkart%2BAssured&sort=popularity&fm=neo%2Fmerchandising&iid=M_e6ae5eb3-cfee-4714-b6e6-bf3eb2572d0a_14.A541W591A5GU&ssid=9r1kdnfgr40000001580014533197&otracker=hp_omu_Big%2BBrands%2Bon%2BBigger%2BDiscounts_3_7.dealCard.OMU_A541W591A5GU_5&otracker1=hp_omu_WHITELISTED_neo%2Fmerchandising_Big%2BBrands%2Bon%2BBigger%2BDiscounts_NA_dealCard_cc_3_NA_view-all_5&cid=A541W591A5GU")
+	// log.Fatal(http.ListenAndServe(":8080", handler))
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", get).Methods(http.MethodGet)
+	r.HandleFunc("/", post).Methods(http.MethodPost)
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
+func get(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application-json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"message":"Get Called!"}`))
+}
+func post(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application-json")
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte(`{"message":"Post Called!"}`))
 }
