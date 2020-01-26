ce35d44e1057906b1c0646a9f460f1ff3805f8c6
FarzamAlam <farzamcse@gmail.com>
2020-01-26 10:14:27 +0530

    Completed the RedirectHandler(), Now we need to break it into two funcs

--- src/main.go
+++ src/main.go
@@ -1,36 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
-	"os"
 	"shorturl"
 )
 
 func main() {
 
-	// dest := "https://golang.org/pkg/net/http"
-	// src := "/gobaby"
+	handler := shorturl.RedirectHandler()
 
-	// srcHash := shorturl.GenerateHash(src)[:8]
-	// fmt.Println(srcHash)
-	count := 0
-	var dest string
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		count++
-		fmt.Printf("Inside handle func: %d\n ", count)
-		dest, _, err := shorturl.FindURL(r.URL.Path[1:])
-		if err != nil {
-			os.Exit(1)
-		}
-		if r.URL.Path == "/" {
-			fmt.Fprintln(w, "Welcome to go-shorurl")
-		} else {
-			fmt.Fprintf(w, "%q Path in NOT FOUND", r.URL.Path)
-		}
-	})
-
-	http.HandleFunc("/", shorturl.RedirectHandler(dest))
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", handler))
 }
--- src/shorturl/handler.go
+++ src/shorturl/handler.go
@@ -3,11 +3,24 @@ package shorturl
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
-func RedirectHandler(dest string) http.HandlerFunc {
+func RedirectHandler() http.HandlerFunc {
 	fmt.Println("Inside RedirectHandler()")
 	return func(w http.ResponseWriter, r *http.Request) {
-		http.Redirect(w, r, dest, http.StatusFound)
+		fmt.Println("Inside the return of RedirectHandler")
+		dest, found, err := FindURL(r.URL.Path[1:])
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error in RedirectHandler() : %v", err)
+			os.Exit(1)
+		}
+		if found {
+			http.Redirect(w, r, dest, http.StatusFound)
+		} else if r.URL.Path == "/" {
+			fmt.Fprintln(w, "Welcome to go-shorturl")
+		} else {
+			fmt.Fprintf(w, "%q Path in NOT FOUND", r.URL.Path)
+		}
 	}
 }
